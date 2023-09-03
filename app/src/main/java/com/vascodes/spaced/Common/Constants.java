6d4752ac8000004b17a471c2eb99900c2b832560
vascodes <nikhilvas12@gmail.com>
2023-09-03 17:26:05 +0530

    Create Utils class and update Constants class.

--- app/src/main/java/com/vascodes/spaced/Common/Constants.java
+++ app/src/main/java/com/vascodes/spaced/Common/Constants.java
@@ -3,4 +3,6 @@ package com.vascodes.spaced.Common;
 public class Constants {
     public static final String DB_NAME = "spaced_db";
     public static final int DB_VERSION = 1;
+    public static final String DECK_TABLE = "Deck";
+    public static final String FLASHCARD_TABLE = "Flashcard";
 }
--- app/src/main/java/com/vascodes/spaced/Common/Utils.java
+++ app/src/main/java/com/vascodes/spaced/Common/Utils.java
@@ -1,8 +1,24 @@
 package com.vascodes.spaced.Common;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.vascodes.spaced.Model.Deck;
+
+import java.util.ArrayList;
+
 public class Utils {
     public static boolean isStringEmpty(String str){
         str = str.trim();
         return str.isEmpty() || str.equals("");
+    }
+
+    public static <T> Spinner fillSpinner(Context context, Spinner spinner, ArrayList<T> dataList){
+        ArrayAdapter<T> arrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, dataList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        return spinner;
     }
 }
